c2a299961907e21c1fda3ea9109a64a59f5d5ea3
Darold So <darold.so@popbo.net>
2020-08-19 17:23:36 +0800

    Modify api calls to use rxjs/ajax

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { ajax } from 'rxjs/ajax';
 
 const apiKey = '1d7008267692450dbc32db7ee51290e7'
 
@@ -6,14 +7,7 @@ const api = {
   getArticlesFromDomains(params) {
     const { domains = [], page = 1, pageSize = 10 } = params
 
-    return axios.get('https://newsapi.org/v2/everything', {
-      params: {
-        domains: domains.toString(),
-        pageSize,
-        page,
-        apiKey
-      }
-    })
+    return ajax.getJSON(`https://newsapi.org/v2/everything?domains=${domains.toString()}&pageSize=${pageSize}&page=${page}&apiKey=${apiKey}`)
   }
 }
 
