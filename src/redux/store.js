893954ed790217020a90f160967e4a72e44b1ad9
Darold So <darold.so@popbo.net>
2020-08-19 17:22:56 +0800

    Setup redux-observable middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,23 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { createEpicMiddleware } from 'redux-observable';
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducers'
+import rootEpic from './epics'
+
+const epicMiddleware = createEpicMiddleware()
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function initStore(preloadedState) {
-  return createStore(
+  const store = createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunkMiddleware)
+    composeEnhancers(
+      applyMiddleware(epicMiddleware)
+    )
   )
+
+  epicMiddleware.run(rootEpic)
+
+  return store
 }
